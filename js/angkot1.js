3de64149f3cad4350c6fb76f57a5cc7da59e9d89
arifwahyu07 <arif.ws8@gmail.com>
2021-01-11 14:20:10 +0700

    Menambahkan Variabel jmlAngkot, merubah variabel angkot jadi noAngkot, merubah hasil tampilan dari angkot jadi Angkot No.

--- js/angkot1.js
+++ js/angkot1.js
@@ -1,5 +1,6 @@
-var angkot = 1;
-while (angkot <=10 ){
-    console.log('angkot '+ angkot + ' beroperasi dengan baik');
-    angkot ++;
+var jmlAngkot =10;
+var noAngkot = 1;
+while (noAngkot <= jmlAngkot){
+    console.log('Angkot No. '+ noAngkot + ' beroperasi dengan baik');
+    noAngkot ++;
 } 
